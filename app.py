b793fcdb05f0e0a0219d7bee21ce4a2eeee9eb0e
ReginoDeang <reginodeangjr@gmail.com>
2023-10-08 22:18:19 +0800

    updated_for_assignment

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ def hello_geek():
     return '<h1>Hello from Flask & Docker</h2>'
 
 
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+# not sure why i cant run localhost 3000 without this code below
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=3000)  
